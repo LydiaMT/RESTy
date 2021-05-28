76064b52c525ed09675fabe159a5b2559de7ebb9
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-05-27 17:28:53 -0700

    nav bar working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Header from './components/header/header'
 import Form from './components/form/form'
 import History from './components/history/history'
+import Help from './components/help/help'
 import Results from './components/results/results'
-import Footer from './components/footer/footer'
 import axios from 'axios'
-import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 class App extends React.Component {
   constructor(props) {
@@ -66,24 +65,37 @@ class App extends React.Component {
   render() {
     return (
       <React.StrictMode>
-        <Form 
-          toggleLoading={this.toggleLoading} 
-          toggleSearchLoading={this.toggleSearchLoading} 
-          handleChange={this.handleChange} 
-          handelClick={this.handelClick}
-          handler={this.handleForm} 
-          handelSubmit={this.handelSubmit}
-          url={this.state.url}
-          method={this.state.method}
-          />
-        <div className="output-wrapper">
-          <History 
-            handleHistory={this.handleHistory}
-            history={this.state.history}/>
-          <Results
-            results={this.state.results} 
-            searchLoading={this.state.searchLoading}/>
-        </div>
+        <Switch>
+          <Route exact path="/">
+            <Form 
+              toggleLoading={this.toggleLoading} 
+              toggleSearchLoading={this.toggleSearchLoading} 
+              handleChange={this.handleChange} 
+              handelClick={this.handelClick}
+              handler={this.handleForm} 
+              handelSubmit={this.handelSubmit}
+              url={this.state.url}
+              method={this.state.method}
+              />
+            <div className="output-wrapper">
+              <History 
+                handleHistory={this.handleHistory}
+                history={this.state.history}/>
+              <Results
+                results={this.state.results} 
+                searchLoading={this.state.searchLoading}/>
+            </div>
+          </Route>
+          <Route exact path="/History">
+            <History 
+              handleHistory={this.handleHistory}
+              history={this.state.history}/>
+          </Route>
+          <Route exact path="/Help">
+            <Help/>
+          </Route>
+        </Switch>
+
       </React.StrictMode> 
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Header from './components/header/header'
 import App from './App.js'
-import History from './components/history/history'
-import Help from './components/help/help'
 import Footer from './components/footer/footer'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 
 const Main = () => {
@@ -12,17 +10,7 @@ const Main = () => {
       <React.StrictMode>
         <BrowserRouter>
           <Header />
-            <Switch>
-              <Route exact path="/">
-                <App/>
-              </Route>
-              <Route exact path="/History">
-                <History/>
-              </Route>
-              <Route exact path="/Help">
-                <Help/>
-              </Route>
-            </Switch>
+          <App />
           <Footer />
         </BrowserRouter>
       </React.StrictMode> 
