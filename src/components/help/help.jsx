2a82efafb0568461c3b72ab877583f8c1856c76b
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-05-30 12:13:04 -0700

    request header/body hidden

--- src/components/help/help.jsx
+++ src/components/help/help.jsx
@@ -8,17 +8,15 @@ const Help = () => {
       <p><span className="special-font">RESTy</span> is an API tool to test your routes and CRUD methods. <span className="special-font">RESTy</span> is simple to use, and stores your past queries for you so you can easily requery your routes and API calls.</p>
       <br/>
       <h5>To use RESTy:</h5>
-      <p>
-        <ol>
-          <li><span className="special-font">1.</span> Enter your route into the URL input on the Home page. For PUT and POST requests, add a body as well. </li>
-          <li><span className="special-font">2.</span> Click the GO! button.</li>
-          <li><span className="special-font">3.</span> Successful calls will return the results of your query. Unsucessful calls will throw an error.</li>
-        </ol>
-      </p>
+      <ol>
+        <li><span className="special-font">1.</span> Enter your route into the URL input on the Home page. For PUT and POST requests, add a body as well. </li>
+        <li><span className="special-font">2.</span> Click the GO! button.</li>
+        <li><span className="special-font">3.</span> Successful calls will return the results of your query. Unsucessful calls will throw an error.</li>
+      </ol>
       <br/>
       <p>It's as simple as that! Navigate to the History page to see a complete selection of your past queries. Click the query to see it's metadata, or click re-run to make the API call again. You can re-run previous API calls from the History or Home page.</p>
       <br/>
-      <p><span className="special-font">PRO TIP:</span> <a href="https://reqres.in/">reqres</a> is great for API tests. </p>
+      <p><span className="special-font">PRO TIP:</span> <a href="https://reqres.in/" rel="noreferrer noopener" target="_blank">reqres</a> is great for API tests. </p>
       <h6>Happy programming!</h6>
     </section>
   )
